d14401a50f0682db8043360e2d8ad7761cae071f
Alicia Flores <floresalvarado.alicia@gmail.com>
2021-06-13 15:40:54 -0500

    renderizado de la data de la api

--- harrypotterapp/src/App.js
+++ harrypotterapp/src/App.js
@@ -5,11 +5,15 @@ import StaffCards from "./components/StaffCards";
 import RenderStaff from "./components/RenderStaff";
 import AllCards from "./components/AllCards";
 import Cards from "./components/Cards";
+import StudentsCards from "./components/StudentsCards";
+import RenderStudents from "./components/RenderStudents";
 
 const App = () => (
   <Provider store={Store}>
     <main>
       <h1>Harry Potter</h1>
+      <StudentsCards />
+      <RenderStudents />
       <StaffCards />
       <RenderStaff />
       <Cards />
